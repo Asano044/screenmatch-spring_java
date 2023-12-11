107559dacb8e6a047f4304c077b3d25083fba5c5
Alvaro Carvalho <alvarocarv2004@gmail.com>
2023-12-11 10:22:05 -0300

    classificando os dados de recebidos de forma personalizada

--- src/main/java/br/com/casasbahia/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/casasbahia/screenmatch/ScreenmatchApplication.java
@@ -1,9 +1,12 @@
 package br.com.casasbahia.screenmatch;
 
+import br.com.casasbahia.screenmatch.model.ConverteDados;
+import br.com.casasbahia.screenmatch.model.DadosSerie;
 import br.com.casasbahia.screenmatch.service.ConsumoApi;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cglib.core.Converter;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -14,9 +17,14 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String...args) {
-		System.out.println("Primeiro projeto Spring sem Web");
 		ConsumoApi consumo = new ConsumoApi();
-		String json = consumo.obterDados("http://www.omdbapi.com/?t=doctor+who&Season=1&Episode=1&apikey=a745f28e");
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dadosSerie;
+		System.out.println("Primeiro projeto Spring sem Web");
+		String json = consumo.obterDados("http://www.omdbapi.com/?t=doctor+who&apikey=a745f28e");
 		System.out.println(json);
+		dadosSerie = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dadosSerie);
+
 	}
 }
