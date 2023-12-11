303d8ab664b05924c2fbcf3323e291a72d63a8d4
Alvaro Carvalho <alvarocarv2004@gmail.com>
2023-12-11 09:07:13 -0300

    criando arquivo de consumir api

--- src/main/java/br/com/casasbahia/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/casasbahia/screenmatch/ScreenmatchApplication.java
@@ -1,5 +1,6 @@
 package br.com.casasbahia.screenmatch;
 
+import br.com.casasbahia.screenmatch.service.ConsumoApi;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,7 +15,8 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	@Override
 	public void run(String...args) {
 		System.out.println("Primeiro projeto Spring sem Web");
-
-
+		ConsumoApi consumo = new ConsumoApi();
+		String json = consumo.obterDados("http://www.omdbapi.com/?t=doctor+who&Season=1&Episode=1&apikey=a745f28e");
+		System.out.println(json);
 	}
 }
