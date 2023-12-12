5151260deeb0434e234a68e43f44b993885b2b7e
Alvaro Carvalho <alvarocarv2004@gmail.com>
2023-12-12 11:43:32 -0300

    exibindo os 5 melhores episódios da série

--- src/main/java/br/com/casasbahia/screenmatch/principal/Principal.java
+++ src/main/java/br/com/casasbahia/screenmatch/principal/Principal.java
@@ -1,11 +1,13 @@
 package br.com.casasbahia.screenmatch.principal;
 
 import br.com.casasbahia.screenmatch.model.ConverteDados;
+import br.com.casasbahia.screenmatch.model.DadosEpisodio;
 import br.com.casasbahia.screenmatch.model.DadosSerie;
 import br.com.casasbahia.screenmatch.model.DadosTemporada;
 import br.com.casasbahia.screenmatch.service.ConsumoApi;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
@@ -13,6 +15,7 @@ public class Principal {
     private Scanner leitura = new Scanner(System.in);
     private ConsumoApi consumo = new ConsumoApi();
     private DadosSerie dadosSerie;
+    private DadosTemporada dadosTemporada;
     private ConverteDados conversor = new ConverteDados();
     private final String ENDERECO = "https://www.omdbapi.com/?t=";
     private final String API_KEY = "&apikey=a745f28e";
@@ -26,11 +29,24 @@ public class Principal {
 
 		List<DadosTemporada> temporadas = new ArrayList<>();
 		for (int i = 1; i <= dadosSerie.totalTemporadas(); i++) {
-			json = consumo.obterDados("https://www.omdbapi.com/?t=doctor+who&Season=" + i +
-					"&apikey=a745f28e");
+			json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + "&Season=" + i +
+					API_KEY);
 			temporadas.add(conversor.obterDados(json, DadosTemporada.class));
 		}
 		temporadas.forEach(System.out::println);
 
+//        Exibindo apenas o nome dos episódios
+//        temporadas.forEach(t -> t.episodio().forEach(e -> System.out.println(e.titulo())));
+
+//        Colocanto todos episódios em uma lista à parte
+        List<DadosEpisodio> dadosEpisodios = temporadas.stream()
+                        .flatMap(t -> t.episodio().stream())
+                                .toList();
+//        Filtrando os 5 melhores episódios da série
+        dadosEpisodios.stream()
+                .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+                .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
+                .limit(5)
+                .forEach(System.out::println);
     }
 }
