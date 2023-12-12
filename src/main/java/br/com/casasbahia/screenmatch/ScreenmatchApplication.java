e52b96f0c743a455bf7cac20cb938cfa9a0fdc76
Alvaro Carvalho <alvarocarv2004@gmail.com>
2023-12-12 09:35:36 -0300

    limpando main e exibindo as temporadas da série

--- src/main/java/br/com/casasbahia/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/casasbahia/screenmatch/ScreenmatchApplication.java
@@ -21,27 +21,5 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	public void run(String...args) {
 		Principal principal = new Principal();
 		principal.exibeMenu();
-//		ConsumoApi consumo = new ConsumoApi();
-//		ConverteDados conversor = new ConverteDados();
-//		DadosSerie dadosSerie;
-//		DadosTemporada dadosTemporada;
-//		DadosEpisodio dadosEpisodio;
-//		System.out.println("Primeiro projeto Spring sem Web");
-//		String json = consumo.obterDados("http://www.omdbapi.com/?t=doctor+who&apikey=a745f28e");
-//
-//		dadosSerie = conversor.obterDados(json, DadosSerie.class);
-//		System.out.println(dadosSerie);
-//
-//		json = consumo.obterDados("https://www.omdbapi.com/?t=doctor+who&Season=1&Episode=1&apikey=a745f28e");
-//		dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
-//		System.out.println(dadosEpisodio);
-//
-//		List<DadosTemporada> temporadas = new ArrayList<>();
-//		for (int i = 1; i <= dadosSerie.totalTemporadas(); i++) {
-//			json = consumo.obterDados("https://www.omdbapi.com/?t=doctor+who&Season=" + i +
-//					"&apikey=a745f28e");
-//			temporadas.add(conversor.obterDados(json, DadosTemporada.class));
-//		}
-//		temporadas.forEach(System.out::println);
 	}
 }
--- src/main/java/br/com/casasbahia/screenmatch/principal/Principal.java
+++ src/main/java/br/com/casasbahia/screenmatch/principal/Principal.java
@@ -1,12 +1,19 @@
 package br.com.casasbahia.screenmatch.principal;
 
+import br.com.casasbahia.screenmatch.model.ConverteDados;
+import br.com.casasbahia.screenmatch.model.DadosSerie;
+import br.com.casasbahia.screenmatch.model.DadosTemporada;
 import br.com.casasbahia.screenmatch.service.ConsumoApi;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Principal {
     private Scanner leitura = new Scanner(System.in);
     private ConsumoApi consumo = new ConsumoApi();
+    private DadosSerie dadosSerie;
+    private ConverteDados conversor = new ConverteDados();
     private final String ENDERECO = "https://www.omdbapi.com/?t=";
     private final String API_KEY = "&apikey=a745f28e";
 
@@ -14,6 +21,16 @@ public class Principal {
         System.out.println("Informe o nome da série: ");
         String nomeSerie = leitura.nextLine();
         String json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
-        System.out.println(json);
+        dadosSerie = conversor.obterDados(json, DadosSerie.class);
+        System.out.println(dadosSerie);
+
+		List<DadosTemporada> temporadas = new ArrayList<>();
+		for (int i = 1; i <= dadosSerie.totalTemporadas(); i++) {
+			json = consumo.obterDados("https://www.omdbapi.com/?t=doctor+who&Season=" + i +
+					"&apikey=a745f28e");
+			temporadas.add(conversor.obterDados(json, DadosTemporada.class));
+		}
+		temporadas.forEach(System.out::println);
+
     }
 }
